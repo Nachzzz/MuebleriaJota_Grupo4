b7613bacd5d9c277c073a313ac0c4db45df7256e
Camila Varela <camila.programacion23@gmail.com>
2025-09-05 21:21:48 -0300

    cambios en producto

--- js/detalle.js
+++ js/detalle.js
@@ -6,20 +6,28 @@ export function detalleProducto(p) {
 
     // Redirigir a la página de detalle
     window.location.href = "producto.html";
-    const contenedor = document.getElementById("catalogo");
-    if (!contenedor) return
-
-    console.log(p)
-    let carrito = JSON.parse(localStorage.getItem("carrito")) || []
-
-    const objeto_producto = {
-        id: p.id,
-        nombre: p.nombre,
-        descripcion: p.descripcion,
-        precio: p.precio,
-        cantidad: 1
-    }
-
-    
-
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const producto = JSON.parse(localStorage.getItem("productoDetalle"));
+    const contenedor = document.querySelector("#detalle");
+
+    if (producto) {
+        const div = document.createElement("div")
+        contenedor.innerHTML = `
+            
+            <h2>${producto.nombre}</h2>
+            <img src="${producto.img}" alt="${producto.nombre}" style="max-width:250px; display:block; margin-bottom:1rem;">
+            <p>${producto.descripcion}</p>
+            <p><strong>Precio:</strong> $${producto.precio}</p>
+            <button id="volver">Volver</button>
+        `;
+
+        document.querySelector("#volver").addEventListener("click", () => {
+            window.history.back();
+        });
+    } else {
+        contenedor.textContent = "❌ Producto no encontrado.";
+    }
+});
